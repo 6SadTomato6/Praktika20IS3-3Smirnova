9526a113e910b2ed1f32f008af7c4a5057d952cc
6SadTomato6 <124986010+6SadTomato6@users.noreply.github.com>
2023-05-26 14:04:00 +0300

    изменено

---
+++ SmirnovaPR7/Domain/Models/ShoppingCart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Models
+{
+    public partial class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+            CartItems = new HashSet<CartItem>();
+        }
+
+        public int CartId { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime CartDate { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public virtual Customer Customer { get; set; } = null!;
+        public virtual ICollection<CartItem> CartItems { get; set; }
+    }
+}
